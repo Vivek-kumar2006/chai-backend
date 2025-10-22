658d9e31e7c958c25e06464b65a9f3ffcd4d7841
Vivek Kumar <vivekkumariiit1509@gmail.com>
2025-10-22 19:45:47 +0530

    User Controller and Bug Fixes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,7 +2,7 @@ const asyncHandler =(requestHandler) =>{
   return  (req,res,next) => {
         Promise.resolve(
             requestHandler(req,res,next)
-        ).reject(
+        ).catch(
             (err) =>next(err)
         )
     }
